ce75c1e1862b03ec9f1ac8fee5eeed4fd3771669
HanaeOukacha1 <hanaeoukacha04@gmail.com>
2025-11-29 00:47:21 +0100

    gitignore

--- build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_hospital-management-cpp-gui_FILE C:/cpp_project/hospital-management-cpp-gui/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/hospital-management-cpp-gui.exe)
-set(__QT_DEPLOY_TARGET_hospital-management-cpp-gui_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_hospital-management-cpp-gui_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
